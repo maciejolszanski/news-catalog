29489fa79e60ad696f836875f2be13021e74ffea
Maciej Olszański <olszanskimaciej@wp.pl>
2023-10-24 17:12:28 +0200

    add runh spider from code

--- news-catalog/main.py
+++ news-catalog/main.py
@@ -2,5 +2,12 @@
 This file contains logic of application
 """
 
+import subprocess
+import os
+
 if __name__ == "__main__":
-    pass
+    
+    spider_name = "wp_spider"
+    parent_dir = os.getcwd()
+    scrapy_path = os.path.join(parent_dir, "scrapy_data")
+    subprocess.run(["scrapy", "crawl", spider_name], cwd=scrapy_path)
