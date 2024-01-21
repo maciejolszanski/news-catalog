7b63f08f82aa0049e86aed10f473e4f187a476f4
Maciej Olszański <olszanskimaciej@wp.pl>
2024-01-21 13:30:54 +0100

    better displayed df

--- news_catalog/streamlit_project/streamlit_main.py
+++ news_catalog/streamlit_project/streamlit_main.py
@@ -2,6 +2,7 @@ import streamlit as st
 from mongoDB_handler import mongoDB_handler
 import pandas as pd
 
+st.set_page_config(layout="wide")
 
 st.title("News Catalog")
 settings = st.secrets["mongo"]
@@ -12,4 +13,14 @@ items = mongodb.get_data()
 df = pd.DataFrame(items, index=list(range(len(items))))
 
 # display without id
-df.iloc[:, 1:]
+column_config = {
+    "_id": None,
+    "title": "Article title",
+    "data": st.column_config.DateColumn(),
+    "lead": None,
+    "text": None,
+    "author": "Author",
+    "url": None,
+}
+
+st.dataframe(df, column_config=column_config)
